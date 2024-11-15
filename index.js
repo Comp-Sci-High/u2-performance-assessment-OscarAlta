553a0f3106056f9332b7e3baff87e02d0ee2475a
Oscar <oscar.altamirano25@compscihigh.org>
2024-11-15 14:59:19 +0000

    Completed the get post

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+async function generaterandomQuote() {
+let api_Key = "ri5p9f0yJojJqR9hbdOkUw==ukSTriZIZMaVrwdZ"
 
+let randomQuote = "https://api.api-ninjas.com/v1/quotes"
+
+const options = {
+  method: "GET",
+  headers: {
+   'X-Api-Key': api_Key,
+    'Content-Type': 'application/json'
+  },
+}
+  
+
+const response = await fetch(randomQuote, options);
+
+  const data = await response.json();
+  console.log(data); 
+  return data;
+}
+
+
+generaterandomQuote();
